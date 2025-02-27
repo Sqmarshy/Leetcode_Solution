72b3ad8211d239fd9c74e961b21d400f21f0adf3
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-02-27 12:03:28 +0800

    Time: 963 ms (48.35%), Space: 18.2 MB (60.26%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.py
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.py
@@ -7,7 +7,12 @@ class Solution:
             for j in range(i+1, len(arr)):
                 curr = 2
                 n1, n2 = arr[i], arr[j]
+
                 while n1 + n2 in lookup:
+                    if (n1, n2) in memo:
+                        curr += memo[(n1, n2)] - 1
+                        break
+                    memo[(n1, n2)] = curr
                     n1, n2 = n2, n1 + n2
                     curr += 1
                 res = res if res > curr else curr
