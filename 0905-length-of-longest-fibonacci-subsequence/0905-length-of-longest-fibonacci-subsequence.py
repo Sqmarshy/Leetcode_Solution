89132a99a63edb6544c179805c2a8b4dbe8069de
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-02-27 11:55:07 +0800

    Time: 993 ms (47.68%), Space: 18.1 MB (64.57%) - LeetHub

---
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        res = 0
+        lookup = set(arr)
+        memo = {}
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                curr = 2
+                n1, n2 = arr[i], arr[j]
+                while n1 + n2 in lookup:
+                    n1, n2 = n2, n1 + n2
+                    curr += 1
+                res = res if res > curr else curr
+        return res if res >= 3 else 0
