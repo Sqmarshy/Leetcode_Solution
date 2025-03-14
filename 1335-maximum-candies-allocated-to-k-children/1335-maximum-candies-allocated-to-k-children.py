1fee4b274e3a8769bcb8f3e49cb0aef936da3760
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-03-14 09:11:46 +0800

    Time: 845 ms (5.09%), Space: 29.7 MB (54%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def helper(limit):
+            res = 0
+            for candy in candies:
+                res += candy // limit
+                if res >= k:
+                    return True
+            return res >= k
+        
+        left = 1
+        right = 10**9
+        while left <= right:
+            mid = (left + right) // 2
+            if helper(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left if helper(left) else left - 1
