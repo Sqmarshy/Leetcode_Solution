c3705a72f3671b4d8f5b7a3759ed12945cf92aea
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-03-15 11:08:42 +0800

    Time: 463 ms (27.31%), Space: 28.4 MB (93.07%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        def helper(cap):
+            idx, count = 0, 0
+            while idx < len(nums):
+                if nums[idx] <= cap:
+                    count += 1
+                    idx += 2
+                else:
+                    idx += 1
+            return count >= k
+        
+        left, right = 1, max(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if helper(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return right
