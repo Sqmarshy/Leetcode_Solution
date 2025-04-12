4856aafedbd4ed231958ea3994ee705aed067de8
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-04-12 12:48:48 +0800

    Time: 781 ms (100%), Space: 23 MB (22.58%) - LeetHub

---
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
@@ -0,0 +1,50 @@
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        if n == 1:
+            return len([i for i in range(1, 10) if i % k == 0])
+
+        palin = []
+        half = n // 2
+        start = int(10 ** (half - 1))
+        end = int(10 ** half - 1)
+        if n % 2 == 1:
+            for i in range(start, end + 1):
+                left = str(i)
+                right = left[::-1]
+                for j in range(0, 10):
+                    p = int(left + str(j) + right)
+                    palin.append(p)
+        else:
+            for i in range(start, end + 1):
+                left = str(i)
+                right = left[::-1]
+                p = int(left + right)
+                palin.append(p)
+
+        checked_k = [num for num in palin if num % k == 0]
+        res = 0
+        visited = set()
+        for i in checked_k:
+            s = str(i)
+            check = ''.join(sorted(s))
+            if check in visited:
+                continue
+            visited.add(check)
+            digits = len(s)
+            count = Counter(s)
+                
+            val = factorial(digits)
+            for _, c in count.items():
+                if c >= 2:
+                    val /= factorial(c)
+            
+            if '0' in count:
+                count['0'] -= 1
+                val_0 = factorial(digits - 1)
+                for _, c in count.items():
+                    if c >= 2:
+                        val_0 /= factorial(c)
+                val -= val_0
+            res += val
+
+        return int(res)
