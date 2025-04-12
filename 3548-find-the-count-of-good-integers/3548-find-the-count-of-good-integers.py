1c787d23f669aca0cd25dc077b53d9c1ebd3e088
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-04-12 12:54:46 +0800

    Time: 777 ms (100%), Space: 22.1 MB (22.58%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
@@ -1,8 +1,10 @@
 class Solution:
     def countGoodIntegers(self, n: int, k: int) -> int:
+        # Base case, if n only has 1 digit
         if n == 1:
             return len([i for i in range(1, 10) if i % k == 0])
 
+        # Generating all possible Palindrome
         palin = []
         half = n // 2
         start = int(10 ** (half - 1))
@@ -13,18 +15,21 @@ class Solution:
                 right = left[::-1]
                 for j in range(0, 10):
                     p = int(left + str(j) + right)
-                    palin.append(p)
+                    if p % k == 0:
+                        palin.append(p)
         else:
             for i in range(start, end + 1):
                 left = str(i)
                 right = left[::-1]
                 p = int(left + right)
-                palin.append(p)
+                if p % k == 0:
+                    palin.append(p)
 
-        checked_k = [num for num in palin if num % k == 0]
+        # Permutate all possible Palindrome to get nums it can form
+        # Kept a visited set to avoid double-checking same digit distribution
         res = 0
         visited = set()
-        for i in checked_k:
+        for i in palin:
             s = str(i)
             check = ''.join(sorted(s))
             if check in visited:
@@ -32,12 +37,14 @@ class Solution:
             visited.add(check)
             digits = len(s)
             count = Counter(s)
-                
+            
+            # All possible permute
             val = factorial(digits)
             for _, c in count.items():
                 if c >= 2:
                     val /= factorial(c)
             
+            # The ones with leading 0 like 012
             if '0' in count:
                 count['0'] -= 1
                 val_0 = factorial(digits - 1)
@@ -46,5 +53,4 @@ class Solution:
                         val_0 /= factorial(c)
                 val -= val_0
             res += val
-
         return int(res)
