39b70338fa56fe461791abb46d0ea29c717f084a
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-05-02 10:44:16 +0800

    Time: 151 ms (73.36%), Space: 22.3 MB (59.87%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.py
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -1,24 +1,33 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
-        n, s = len(dominoes), list(dominoes)
-        L = [idx for idx in range(n) if s[idx] == 'L']
-        R = [idx for idx in range(n) if s[idx] == 'R']
-        visited = set()
-        while L or R:
-            for _ in range(len(L)):
-                dom = L.pop(0)
-                if (dom - 1 >= 0) and (s[dom - 1] == '.') and (dom - 1 not in visited):
-                    if dom - 2 < 0 or s[dom - 2] != 'R':
-                        L.append(dom - 1)
-                        s[dom - 1] = 'L'
-                    else:
-                        visited.add(dom - 1)
-            for _ in range(len(R)):
-                dom = R.pop(0)
-                if (dom + 1 < n) and (s[dom + 1] == '.') and (dom + 1 not in visited):
-                    if dom + 2 <= n or dom + 2 != 'L':
-                        R.append(dom + 1)
-                        s[dom + 1] = 'R'
-                    else:
-                        visited.add(dom + 1)
-        return ''.join(s)
+        n = len(dominoes)
+        dist = [0] * n
+
+        # Right 
+        curr = 0
+        for r in range(n):
+            if dominoes[r] == '.':
+                if curr == 0: continue
+                dist[r] += curr
+                curr -= 1
+            elif dominoes[r] == 'R':
+                dist[r] = n
+                curr = n - 1
+            else:
+                curr = 0
+
+        # Left
+        curr = 0
+        for l in range(n - 1, -1, -1):
+            if dominoes[l] == '.':
+                if curr == 0: continue
+                dist[l] += curr
+                curr += 1
+            elif dominoes[l] == 'L':
+                dist[l] = -n
+                curr = -(n - 1)
+            else:
+                curr = 0
+        
+        res = ['L' if d < 0 else 'R' if d > 0 else '.' for d in dist]
+        return ''.join(res)
