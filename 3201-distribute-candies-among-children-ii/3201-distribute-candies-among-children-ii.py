3babca52ad91cbb20cf917b3b020441a77a3c80d
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-06-01 15:51:11 +0800

    Time: 4469 ms (10.96%), Space: 17.8 MB (48.63%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
@@ -8,8 +8,8 @@ class Solution:
             if remain > limit * 2:
                 continue # At least one of the 2 remaining child will get > limit candies
             second_min = max(0, remain - limit) # if remain < limit, second child can get any amt <= limit
-            second_max = min(remain, limit)
-            count = second_max - second_min + 1
+            second_max = min(remain, limit) # Second child can take up to remain or limit, whichever is smaller
+            count = second_max - second_min + 1 # Formula to count number of item
             res += count
         
         return res
