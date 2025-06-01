0417affa18fa7733aa0c67401d1f8e66fc50358a
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-06-01 15:49:50 +0800

    Time: 4646 ms (10.28%), Space: 17.6 MB (69.18%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        res = 0
+
+        # First child can either take up to n or up to limit
+        for i in range(min(limit, n) + 1):
+            remain = n - i
+            if remain > limit * 2:
+                continue # At least one of the 2 remaining child will get > limit candies
+            second_min = max(0, remain - limit) # if remain < limit, second child can get any amt <= limit
+            second_max = min(remain, limit)
+            count = second_max - second_min + 1
+            res += count
+        
+        return res
+
+
