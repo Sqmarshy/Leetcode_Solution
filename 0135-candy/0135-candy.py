c6f2d89681b4de98de7e7d964918d514fe7cafeb
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-06-02 14:25:53 +0800

    Time: 47 ms (5.5%), Space: 21.7 MB (5.1%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -4,6 +4,7 @@ class Solution:
         if n == 1:
             return 1
         res = [1] * n
+        total = 0
         queue = deque(sorted([(rate, i) for i, rate in enumerate(ratings)]))
         while queue:
             val, idx = queue.popleft()
@@ -20,7 +21,8 @@ class Solution:
                 res[idx] = res[idx - 1] + 1 
             elif right < val:
                 res[idx] = res[idx + 1] + 1
+            
+            total += res[idx]
         
-        # print(res)
-        return sum(res)
+        return total
             
