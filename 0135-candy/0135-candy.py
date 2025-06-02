cfc8e10c2ea152c98788eaf9457f86ba1e8f7929
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-06-02 14:25:08 +0800

    Time: 39 ms (6.71%), Space: 21.5 MB (5.1%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,26 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        if n == 1:
+            return 1
+        res = [1] * n
+        queue = deque(sorted([(rate, i) for i, rate in enumerate(ratings)]))
+        while queue:
+            val, idx = queue.popleft()
+            if idx == 0:
+                left, right = ratings[idx], ratings[idx + 1]
+            elif idx == n - 1:
+                left, right = ratings[idx - 1], ratings[idx]
+            else:
+                left, right = ratings[idx - 1], ratings[idx + 1]
+            
+            if left < val and right < val:
+                res[idx] = max(res[idx - 1], res[idx + 1]) + 1
+            elif left < val:
+                res[idx] = res[idx - 1] + 1 
+            elif right < val:
+                res[idx] = res[idx + 1] + 1
+        
+        # print(res)
+        return sum(res)
+            
