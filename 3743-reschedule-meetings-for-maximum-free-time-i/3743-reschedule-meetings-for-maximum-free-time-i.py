93704e134b86e1128fd87ebc31227439f2de5220
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-07-09 17:20:50 +0800

    Time: 39 ms (83.65%), Space: 36 MB (87.45%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.py
@@ -1,14 +1,18 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        # Variables
         free = []
         prev = 0
 
+        # All gaps between 2 events
+        # 0 is included as well as it denote there is an extra event there to be shifted
         for i in range(len(startTime)):
             curr = startTime[i] - prev
             free.append(curr)
             prev = endTime[i]
-
         free.append(eventTime - prev)
+
+        # Since relative order need to be preserved, sliding window can be used
         res = max(free) if free else 0
         temp, left = 0, 0
         for i in range(len(free)):
