9412af28446e6455532b0900f7a26a5e73375bf1
SHEN SHU-HSUAN <130201817+Sqmarshy@users.noreply.github.com>
2025-08-27 15:06:28 +0800

    Time: 91 ms (94.35%), Space: 27 MB (93.4%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
@@ -1,35 +1,25 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        if len(costs) == k:
-            return sum(costs)
-
-        first = [(costs[i], i) for i in range(candidates)]
-        last = [(costs[i], i) for i in range(len(costs) - candidates, len(costs))]
+        first = costs[:candidates]
+        last = costs[max(candidates, len(costs) - candidates):]
         heapq.heapify(first)
         heapq.heapify(last)
-        track = set()
         left, right = candidates, len(costs) - candidates - 1
         res = 0
         while k:
-            a = first[0] if first else (9999999, len(costs))
-            b = last[0] if last else (9999999, len(costs))
-            if a < b:
+            a = first[0] if first else 9999999
+            b = last[0] if last else 9999999
+            if a <= b:
                 a = heapq.heappop(first)
-                if a[1] in track:
-                    continue
-                res += a[0]
-                track.add(a[1])
+                res += a
                 if left <= right:
-                    heapq.heappush(first, (costs[left], left))
+                    heapq.heappush(first, costs[left])
                     left += 1
             else:
                 b = heapq.heappop(last)
-                if b[1] in track:
-                    continue
-                res += b[0]
-                track.add(b[1])
+                res += b
                 if left <= right:
-                    heapq.heappush(last, (costs[right], right))
+                    heapq.heappush(last, costs[right])
                     right -= 1
             k -= 1
         return res
